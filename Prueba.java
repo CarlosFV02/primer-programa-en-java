8191d7fb1108455c75cf371e2b8c8cfdf8758e7d
Carlos <carlosfv2002@gmail.com>
2021-10-15 09:28:20 +0200

    Cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,10 @@ public class Prueba {
 
     public static void main(String args[]){
 
-	System.out.println("Carlos Fidalgo Vega");
+      //System.out.println("Carlos Fidalgo Vega");
 	System.out.println("León, 29/12/2002");
 	System.out.println("Cembranos, Windows");
+	System.out.println("Jason Derulo");
 
     }
 
